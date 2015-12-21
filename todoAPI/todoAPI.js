3ccf65d399197ce2e15dd62526406f3cc5996b7c
Raghav Dua <duaraghav8@gmail.com>
2015-12-21 14:50:00 +0530

    Update todoAPI.js

--- todoAPI/todoAPI.js
+++ todoAPI/todoAPI.js
@@ -3,39 +3,38 @@
 	usage:
 	curl http://127.0.0.1:3000/api -X GET
 	curl http://127.0.0.1:3000/api/1 -X GET
+	curl http://127.0.0.1:3000/api/1 -X PUT -H "content-type: application/json" -d "{\"name\" : \"raghav dua\"}"
 */
 
 var express = require ('express');
 var bodyParser = require ('body-parser');
+var app = express ();
 var router = express.Router ();
 var port = 3000;
-var app = express ();
-
-var items = {
-	1 : {
-		'task' : 'Learn MEAN',
-		'deadline' : 'January'
-	},
-	2 : {
-		'task' : 'GSOC',
-		'deadline' : 'January'
-	}
-};
+var items = {};
 
 router.use (bodyParser.json ());
-router.use (bodyParser.urlencoded ({
-	extended : true
-}));
-
-router.route ('/api')
+router.route ('/')
 	.get (function (req, res) {
+		console.log (req.originalUrl + ' called GET');
 		res.send (items);
+	})
+	.delete (function (req, res) {
+		items = {};
+		console.log ('items cleared');
+		res.send ('Items cleared');
 	});
-router.route ('/api/:itemID')
+router.route ('/:itemID')
 	.get (function (req, res) {
-		res.send (items [req.params ['itemID']]);
+		console.log ('Get item ', req.params ["itemID"]);
+		res.send (items [req.params ["itemID"]], '\n');
+	})
+	.put (function (req, res) {
+		console.log ('update item ', req.params ["itemID"]);
+		items [req.params ["itemID"]] = req.body;
+		res.send (req.params ["itemID"], ' updated successfully\n');
 	});
-
-app.use ('/', router);
+app.use ('/api', router);
 app.listen (port);
+
 console.log ('Server up and running');
