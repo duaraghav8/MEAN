7a0ed39f0bbde83a173ad79b31cbd16772655b25
duaraghav8@gmail <duaraghav8@gmail.com>
2016-01-30 12:09:27 +0530

    Initial Commit

---
+++ ngResource/server.js
@@ -0,0 +1,16 @@
+var express = require ('express'),
+	serveStatic = require ('serve-static'),
+	app = express (),
+	port = 8080;
+
+app.use (serveStatic (__dirname));
+app.get ('/user/:name', function (req, res) {
+	console.log (req.params.name);
+	res.json ({
+		name : 'raghav',
+		age : 20
+	});
+})
+	.listen (port, function () {
+		console.log ('uptime');
+	});
