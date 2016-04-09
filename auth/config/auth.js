082e434b2e87112ffbba8e7ee5645e52e9a0026c
duaraghav8@gmail <duaraghav8@gmail.com>
2016-04-09 22:07:26 +0530

    D3 bubble plot: linear & category10 scaling

--- auth/config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	facebookAuth: {
-		'clientID': '605380609613298',
-		'clientSecret': 'b0d6342a637499a8c1bf0e83c781177d',
-		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
-	}
-};
